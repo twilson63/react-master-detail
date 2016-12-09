213400334350626041d817e3bf3b7a8009f7a2d6
Tom Wilson <tom@jackhq.com>
2016-12-09 15:44:35 -0500

    filter categories

--- web/src/pages/categories/index.js
+++ web/src/pages/categories/index.js
@@ -1,24 +1,42 @@
 const React = require('react')
 const { Link } = require('react-router')
 const data = require('../../data')()
+const {filter, propEq} = require('ramda')
 
 const Categories = React.createClass({
   getInitialState() {
     return {
-      categories: []
+      categories: [],
+      filtered: []
     }
   },
   componentDidMount() {
     data.list('categories')
-      .then(categories => this.setState({categories}))
+      .then(categories => this.setState({
+        filtered: categories,
+        categories
+      }))
+  },
+  filter(e) {
+    this.setState({ filtered:
+      filter(
+        category => category.name.indexOf(e.target.value) === 0,
+        this.state.categories
+      )
+    })
   },
   render() {
     return (
       <div>
         <h1>Categories</h1>
         <Link to="/categories/new">New Category</Link>
+        <br />
+        <input
+          onChange={this.filter}
+          placeholder="search" type="text"
+         />
         <ul>
-          {this.state.categories.map(cat => <li>{cat.name}</li>)}
+          {this.state.filtered.map(cat => <li>{cat.name}</li>)}
         </ul>
       </div>
     )
